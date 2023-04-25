3b1add4eea3218439de8f542e00223074eceb784
Clément Brutti-Mairesse <clement.brutti.mairesse@gmail.com>
2023-04-25 18:03:19 +0200

    update

---
+++ coding_test/coding_test/sql_commons.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+DB_NAME = "tutorial.db"
+
+
+class DbConnection(object):
+    def __init__(self, db_name=DB_NAME):
+        self.connection = sqlite3.connect(db_name)
+
+    def __enter__(self):
+        return self.connection, self.connection.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.connection.close()
