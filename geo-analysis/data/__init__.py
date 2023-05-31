5dcfaa34ea5d3cc00422b7f5012104ec26e6b493
Clément Brutti-Mairesse <clement.brutti.mairesse@gmail.com>
2023-05-31 14:51:58 +0200

    update

--- geo-analysis/data/__init__.py
+++ geo-analysis/data/__init__.py
@@ -2,4 +2,5 @@ from pathlib import Path
 
 ROOT = Path(__file__).parent
 
-SELL_PRICES = ROOT / "full.csv"
+SOLD_HOUSE_PRICES = ROOT / "full.csv"
+ELECTRICAL_SUBSTATIONS = ROOT / "poste-source.geojson"
